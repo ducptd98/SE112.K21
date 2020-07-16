592cc0333c9ca90b4c67036cdc44cffc6c17ff7f
Chau Hoang <chau.ducgiang@gmail.com>
2020-07-16 22:53:17 +0700

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -88,7 +88,7 @@ class PostController extends Controller
             'status'=> 200,
             'message'=> 'Comment created successfully',
             'data'=>$post,
-            'data'=>$post->comments,
+            'comments'=>$post->comments,
             'user'=>$post->user
         ]);
     }
